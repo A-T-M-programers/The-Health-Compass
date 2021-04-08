ebb7dbec94bc218ccb091b582153cbe664ac4eb5
MohammadKherYadj <mohammadkher509@gmail.com>
2021-04-08 17:46:46 +0300

    First Work

--- app/src/main/java/com/example/the_health_compass/MainActivity.java
+++ app/src/main/java/com/example/the_health_compass/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.the_health_compass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_up);
     }
 }
