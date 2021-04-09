f532b89c09e991c7cd61ee7198b2abd87e72b3e0
MohammadKherYadj <mohammadkher509@gmail.com>
2021-04-09 20:02:46 +0300

    New Work

--- app/src/main/java/com/example/the_health_compass/sign_up.java
+++ app/src/main/java/com/example/the_health_compass/sign_up.java
@@ -3,15 +3,12 @@ package com.example.the_health_compass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.activity_sign_up);
     }
 }
---
+++ app/src/main/java/com/example/the_health_compass/sing_in.java
@@ -0,0 +1,31 @@
+package com.example.the_health_compass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class sing_in extends AppCompatActivity {
+
+    private TextView create_account ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        create_account =(TextView)findViewById(R.id.tv_create_account);
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateAccountPage();
+            }
+        });
+    }
+    public void openCreateAccountPage()
+    {
+        Intent intent = new Intent(this,sign_up.class);
+        startActivity(intent);
+    }
+}
