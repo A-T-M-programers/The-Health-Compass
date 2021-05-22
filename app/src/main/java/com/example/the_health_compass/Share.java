0104ec126736aa3edd09063da9bfbea76a08d90b
Tofiq <tofikdaowd@gmail.com>
2021-05-22 16:19:55 +0300

    commit

--- app/src/main/java/com/example/the_health_compass/Share.java
+++ app/src/main/java/com/example/the_health_compass/Share.java
@@ -9,13 +9,15 @@ public class Share {
     protected String Phone_Mobile,Phone_Earth,Password,Email,Check_Email;
     protected Image Personal_Image;
     protected boolean Blocking;
+    int id = 0;
     public void InputShare(HashMap<String,String> sick, boolean Blocking){
         this.Blocking = Blocking;
         this.Check_Email = sick.get("Check_Email");
         this.Email = sick.get("Email");
-        this.ID = "1";
+        this.ID = String.valueOf(id);
         this.Password = sick.get("Password");
         this.Phone_Earth = sick.get("Phone_Earth");
         this.Phone_Mobile = sick.get("Phone_Mobile");
+        id++;
     }
 }
