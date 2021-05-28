6a9b8768040878386e5a24e7410f9aa363724a87
MohammadKherYadj <mohammadkher509@gmail.com>
2021-05-28 10:51:06 +0300

    28/5/2021

--- app/src/main/java/com/example/the_health_compass/Validation.java
+++
@@ -1,9 +0,0 @@
-package com.example.the_health_compass;
-
-public class Validation {
-    public static void Validate(String txt) {
-        if (txt.isEmpty()) {
-            return;
-        }
-    }
-}
