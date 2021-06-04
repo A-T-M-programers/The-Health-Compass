9693bf6a307a243ea8ad79151ee4441315ac7f33
MohammadKherYadj <mohammadkher509@gmail.com>
2021-06-04 09:53:39 +0300

    4/6/2021

---
+++ app/src/main/java/com/example/the_health_compass/Settings_page.java
@@ -0,0 +1,20 @@
+package com.example.the_health_compass;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Settings_page extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_settings_page,container,false);
+    }
+}
