c04136204eda0650b391e05dcb5052aee2a314d6
MohammadKherYadj <mohammadkher509@gmail.com>
2021-06-05 19:14:29 +0300

    5/6/2021

--- app/src/main/java/com/example/the_health_compass/sign_up.java
+++ app/src/main/java/com/example/the_health_compass/sign_up.java
@@ -3,12 +3,42 @@ package com.example.the_health_compass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class sign_up extends AppCompatActivity {
-
+    Button btn_Create_Account;
+    AwesomeValidation awesomeValidation ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        btn_Create_Account = (Button)findViewById(R.id. btn_create_account);
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this,R.id.et_full_name, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+        awesomeValidation.addValidation(this,R.id.et_email, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+        awesomeValidation.addValidation(this,R.id.et_check_email,Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+        awesomeValidation.addValidation(this,R.id.ed_password,".{6,}",R.string.invalid_password);
+        awesomeValidation.addValidation(this,R.id.ed_config_password,R.id.et_password,R.string.invalid_confirm_password);
+        awesomeValidation.addValidation(this,R.id.et_mobile_phone,Patterns.PHONE,R.string.invalid_mobile_phone_number);
+        awesomeValidation.addValidation(this,R.id.ed_birthday,"^(?:(?:31(\\\\/|-|\\\\.)(?:0?[13578]|1[02]))\\\\1|(?:(?:29|30)(\\\\/|-|\\\\.)(?:0?[1,3-9]|1[0-2])\\\\2))(?:(?:1[6-9]|[2-9]\\\\d)?\\\\d{2})$|^(?:29(\\\\/|-|\\\\.)0?2\\\\3(?:(?:(?:1[6-9]|[2-9]\\\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\\\d|2[0-8])(\\\\/|-|\\\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\\\4(?:(?:1[6-9]|[2-9]\\\\d)?\\\\d{2})$",R.string.invalid_birthday);
+        btn_Create_Account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(awesomeValidation.validate()){
+                    Toast.makeText(getApplicationContext(),"بيانات صحيحة",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"بيانات خاطئة",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
